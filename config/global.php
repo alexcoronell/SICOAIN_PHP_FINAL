4ac0b940c1232a8a30a405f22f512399506503b1
Alex Coronell <alexcoronell@gmail.com>
2020-07-19 20:25:12 -0500

    Configurada conexión a base de datos

---
+++ config/global.php
@@ -0,0 +1,21 @@
+<?php
+
+//Ip del servidor de base de datos
+define("DB_HOST", "localhost");
+
+//Nombre de la base de datos
+define("DB_NAME", "SICOAIN");
+
+// Usuario de la base de datos
+define("DB_USENAME", "Sicoain");
+
+// Contraseña del usuario de base de datos
+define("DB_PASSWORD", "Sicoain2020+");
+
+// Definimos la codificación de los caracteres
+define("DB_ENCODE", "utf8");
+
+// Definimos la constante como nombre del Proyecto
+define("PRO-NAME", "Sicoain");
+
+?>
