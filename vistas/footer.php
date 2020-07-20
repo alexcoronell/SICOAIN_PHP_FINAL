fc8dcd913724537647436fdf13ebd15a8ff5192d
Alex Coronell <alexcoronell@gmail.com>
2020-07-19 20:55:14 -0500

    Incorporación de mi plantilla HTML Sicoain

---
+++ vistas/footer.php
@@ -0,0 +1,8 @@
+<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="../public/js/jquery-3.5.1.min.js"></script>
+    <script src="../public/js/popper.min.js"></script>
+    <script src="../public/js/bootstrap.min.js"></script>
+</body>
+
+</html>
