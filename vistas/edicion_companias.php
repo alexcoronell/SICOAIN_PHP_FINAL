aa4f12b4e96be07bda360f6b536b54d17c40ca01
Alex Coronell <alexcoronell@gmail.com>
2020-07-23 09:23:09 -0500

    Terminada página edición de compañías

--- vistas/edicion_companias.php
+++ vistas/edicion_companias.php
@@ -3,7 +3,6 @@
 require 'header.php';
 
 ?>
-
     <div class="container-fluid main-box">
         <div class="title-page">
             <h1>SICOAIN - Sistema de Control de Accidentes e Incidentes - Administración de Compañías</h1>
@@ -19,9 +18,9 @@ require 'header.php';
                         <nav>
                             <ul>
                                 <li>Administración de Compañías
-                                    <ul>
-                                        <li><a href="creacion_companias.php" class="active">Creación de Compañías</a></li>
-                                        <li><a href="edicion_companias.php">Edición de Compañías</a></li>
+                                <ul>
+                                        <li><a href="creacion_companias.php">Creación de Compañías</a></li>
+                                        <li><a href="edicion_companias.php" class="active">Edición de Compañías</a></li>
                                         <li><a href="act_desact_companias.php">Act/Desact de Compañías</a></li>
                                     </ul>
                                 </li>
@@ -32,44 +31,44 @@ require 'header.php';
                 </div>
                 <div class="col-8">
                     <div class="box-formulario container mt-5">
-                        <h2 class="text-center title-formularios">Creación de Compañías</h2>
-                        <form name="formulario" id="formulario" method="POST">
-                            <div class="form-group row">
-                                <label for="buscarId" class="col-4 deshabilitado">Buscar:</label>
-                                <div class="col-8">
-                                    <input type="text" class="form-control" name="buscarId" id="buscarId" disabled>
+                        <h2 class="text-center title-formularios">Edición de Compañías</h2>
+                        <div class="form-group row">
+                                <label for="rol" class="col-4">Buscar cargo:</label>
+                                <div class="col-8 searchbox">
+                                <input type="search" class="form-control" name="buscarId" id="buscarId">
+                                <button type="button" class="btnBusqueda" id="btnBusqueda" onclick="buscar()"><i class="fa fa-search" aria-hidden="true"></i></button>
                                 </div>
                             </div>
+                            <form name="formulario" id="formulario" method="POST">
                             <div class="form-group row">
-                                <label for="compania" class="col-4">Compañía: *</label>
+                                <label for="compania" class="col-4">Compañía:</label>
                                 <div class="col-8">
-                                <input type="hidden" name="id" id="id">
+                                    <input type="hidden" name="id" id="id">
                                     <input type="text" class="form-control" name="compania" id="compania">
                                 </div>
                             </div>
 
 
                             <div class="form-group row">
-                                <label for="compania_telefono" class="col-4">Teléfono:*</label>
+                                <label for="telefono_compania" class="col-4">Teléfono:</label>
                                 <div class="col-8">
                                     <input type="text" class="form-control" name="telefono_compania" id="telefono_compania">
                                 </div>
                             </div>
 
                             <div class="form-group row">
-                                <label for="compania_direccion" class="col-4">Dirección: *</label>
+                                <label for="direccion_compania" class="col-4">Dirección:</label>
                                 <div class="col-8">
                                     <textarea name="direccion_compania" id="direccion_compania" maxlength="256" class="form-control"></textarea>
                                 </div>
                             </div>
 
-                            <!-- Botones de formulario -->
                             <div class="form-group row">
                                 <div class="offset-4 col-4">
-                                    <button type="submit" class="btn btn-primary" id="btnGuardar">Guardar</button>
+                                    <button type="submit" class="btn btn-primary">Guardar</button>
                                 </div>
                                 <div class="col-4">
-                                    <button type="button" class="btn btn-light" id="btnCancelar" onclick="limpiar()">Cancelar</button>
+                                    <button type="button" class="btn btn-light">Cancelar</button>
                                 </div>
                             </div>
 
