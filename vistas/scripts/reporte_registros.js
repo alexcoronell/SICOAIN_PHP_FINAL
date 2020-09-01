73a35ce130cb83abac18ee79e825023a27e7d06f
Alex Coronell <alexcoronell@gmail.com>
2020-09-01 16:51:15 -0500

    Creación de Reporte PDF de Incidentes

--- vistas/scripts/reporte_registros.js
+++ vistas/scripts/reporte_registros.js
@@ -9,14 +9,30 @@ function init() {
 function listar() {
     tabla = $('#tbllistado').dataTable({
         language: {
-            url: '../public/datatables/Spanish.json'
+            url: '../public/DataTables/Spanish.json',
+            buttons: {
+                copyTitle: 'Copiado al portapapeles',
+                copyKeys: 'Use your keyboard or menu to select the copy command',
+                copySuccess: {
+                    1: "Copiada una fila al portapapeles",
+                    _: "Copiadas %d filas al portapapeles"
+                }
+            }
         },
         "aProcessing": true, // Activación del procesamiento del datatables
         "aServerSide": true, // Paginación y filtrado realizado por el servidor
         dom: 'Bfrtip', // Se definen los elementos de contcargo de la tabla
-        buttons: [
-            'pdf'
-        ],
+        buttons: ['colvis', 'copy', 'excel', {
+            extend: 'pdf',
+            orientation: 'landscape',
+            pageSize: 'LETTER',
+            download: 'open',
+            title: 'Reporte de Incidentes',
+            exportOptions: {
+                columns: ':visible'
+            },
+            alignment: 'center'
+        }, ],
         "ajax": {
             url: '../ajax/registros.php?op=listar',
             type: "get",
@@ -25,8 +41,12 @@ function listar() {
                 console.log(e.responseText);
             }
         },
+        keys: true,
+        select: true,
+        colReorder: true,
+        rowReorder: true,
         "bDestroy": true,
-        "iDisplayLength": 5, // Paginación
+        "iDisplayLength": 8, // Paginación
         "order": [
             [0, "desc"]
         ] // Ordenación (Columna, Orden)
