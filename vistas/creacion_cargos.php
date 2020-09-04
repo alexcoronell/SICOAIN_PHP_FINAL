41dfb1673ec876bba6b39d9a542b5f7a2147cbb5
Alex Coronell <alexcoronell@gmail.com>
2020-09-04 10:15:23 -0500

    Diseñada versión mobile en gestión de cargos

--- vistas/creacion_cargos.php
+++ vistas/creacion_cargos.php
@@ -15,8 +15,8 @@ if (!isset($_SESSION['nombre'])) {
 
         <div class="main-content container-fluid">
             <div class="row">
-                <div class="col-4">
-                    <div class="menu-box">
+                <div class="col-12 col-md-4">
+                    <div class="menu-box menu-box-modulos">
                         <div class="title-menu">
                             <h2>Menú de Empleados</h2>
                         </div>
@@ -41,41 +41,41 @@ if (!isset($_SESSION['nombre'])) {
                         <button class="btn btn-light salir-menu"><a href="principal.php">Regresar</a></button>
                     </div>
                 </div>
-                <div class="col-8">
+                <div class="col-12 col-md-8">
                     <div class="box-formulario container mt-1 ml-1">
                         <h2 class="text-center title-formularios">Creación de Cargos</h2>
                         <form name="formulario" id="formulario" method="POST">
+
                             <div class="form-group row">
-                                <label for="buscarcargo" class="col-4 deshabilitado">Buscar cargo:</label>
-                                <div class="col-8">
-                                    <input type="search" class="form-control" name="buscarId" id="buscarId" disabled>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="cargo" class="col-4">Nombre Cargo:*</label>
-                                <div class="col-8">
+                                <label for="cargo" class="col-12 col-md-4">Nombre Cargo:*</label>
+                                <div class="col-12 col-md-8">
                                     <input type="hidden" name="id" id="id">
                                     <input type="text" class="form-control" maxlength="30" name="cargo" id="cargo" required>
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="descripcion" class="col-4">Descripción:</label>
-                                <div class="col-8">
+                                <label for="descripcion" class="col-12 col-md-4">Descripción:</label>
+                                <div class="col-12 col-md-8">
                                     <textarea name="descripcion" id="descripcion" maxlength="256" class="form-control" required></textarea>
                                 </div>
                             </div>
 
                             <!-- Botones de formulario -->
                             <div class="form-group row">
-                                <div class="offset-4 col-4">
+                                <div class="col-6 offset-md-4 col-md-4 guardar">
                                     <button type="submit" class="btn btn-primary" id="btnGuardar">Guardar</button>
                                 </div>
-                                <div class="col-4">
+                                <div class="col-6 col-md-4 cancelar text-right text-md-left">
                                     <button type="button" class="btn btn-light" onclick="limpiar()">Cancelar</button>
                                 </div>
                             </div>
 
                         </form>
+
+                        <!-- Boton regresar en vista mobile -->
+                        <div class="boton-mobile-regresar col-12 row mb-2">
+                            <button class="btn btn-light px-5"><a href="empleados.php"><i class="fa fa-arrow-left pr-2" aria-hidden="true"></i>Regresar</a></button>
+                        </div>
                     </div>
                 </div>
             </div>
