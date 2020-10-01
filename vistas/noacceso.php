954fb26260a0d38c511dd5a78b4549bcd4accf33
Alex Coronell <alexcoronell@gmail.com>
2020-10-01 12:16:25 -0500

    Se agrega menú principal

--- vistas/noacceso.php
+++ vistas/noacceso.php
@@ -3,8 +3,20 @@
             <div class="col-12 col-sm-8 col-md-6 col-lg-4 col-xl-3">
                 <div class="menu-box-noacceso">
                     <div class="title-menu">
-                        <h2>Menú</h2>
+                        <h2>Menú Principal</h2>
                     </div>
+                    <nav>
+                        <ul>
+                            <li><a href="usuarios.php">Usuarios</a></li>
+                            <li><a href="empleados.php">Empleados y Cargos</a></li>
+                            <li><a href="companias.php">Compañías</a></li>
+                            <li><a href="sedes.php">Sedes</a></li>
+                            <li><a href="eps.php">EPS</a></li>
+                            <li><a href="arl.php">ARL</a></li>
+                            <li><a href="menu_incidentes.php">Administración de Incidentes</a></li>
+                            <li><a href="menu_reportes.php">Reportes</a></li>
+                        </ul>
+                    </nav>
 
                     <button class="btn btn-light salir-menu mt-2"><a href="principal.php"><i class="fa fa-arrow-left pr-2" aria-hidden="true"></i>Regresar</a></button>
                 </div>
